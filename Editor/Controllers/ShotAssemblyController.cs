e5bc2dc76ec9fcbb227d7f2628092ead482ae76a
Mike Wuetherick <mikew@unity3d.com>
2020-02-07 13:38:13 -0800

    first pass on the wizard

---
+++ Editor/Controllers/ShotAssemblyController.cs
@@ -0,0 +1,186 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace PixelWizards.ShotAssembly
+{
+    /// <summary>
+    /// central data model for everything we need
+    /// </summary>
+    public class ShotAssemblyModel
+    {
+        public string sceneName = "AnimReview";
+        public string scenePath = "Scenes";
+        public SceneAsset sceneRef;
+        public string timelineName = "MasterTimeline";
+        public string timelinePath = "Timeline";
+        public TimelineAsset timelineRef;
+        public GameObject actorPrefab;
+        public GameObject actorSceneInstance;
+        public string animationPath = "";
+        public string animFilter;
+
+        public List<AnimationClip> animationList = new List<AnimationClip>();
+    }
+
+    public class ShotAssemblyController
+    {
+        public static ShotAssemblyModel model = new ShotAssemblyModel();
+
+        /// <summary>
+        /// Initializes the data model when the window is opened
+        /// </summary>
+        public static void Init()
+        {
+            model = new ShotAssemblyModel();
+        }
+
+        /// <summary>
+        /// Find all of the animations in the specified path & filter
+        /// </summary>
+        public static void RefreshAnimationList()
+        {
+            model.animationList = GetAnimations();
+        }
+
+        /// <summary>
+        /// Finds all of the animationClips in the project, optionally filtered
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static List<AnimationClip> GetAnimations()
+        {
+            var searchPath = "Assets";
+            if( model.animationPath.Length != 0)
+            {
+                searchPath += "/" + model.animationPath;
+            }
+            var animList = new List<AnimationClip>();
+            var guids = AssetDatabase.FindAssets( model.animFilter + " t:AnimationClip", new[] { searchPath });
+            foreach( var guid in guids)
+            {
+                var animPath = AssetDatabase.GUIDToAssetPath(guid);
+                var anim = (AnimationClip)AssetDatabase.LoadAssetAtPath( animPath, typeof(AnimationClip));
+               // Debug.Log("Anim: " + anim.name);
+                animList.Add(anim);
+            }
+
+            return animList;
+        }
+
+        /// <summary>
+        /// dbl checks to see that the user filled in the appropriate info: TODO: pop a modal to display the error output instead of console log
+        /// </summary>
+        /// <returns></returns>
+        public static bool ValidateData()
+        {
+            var validateLog = new StringBuilder();
+
+            if( model.actorPrefab == null)
+            {
+                validateLog.AppendLine("Need to specify an Actor (Skinned Mesh Renderer) to assign the animations to!");
+            }
+            if (model.animationList.Count < 1)
+            {
+                validateLog.AppendLine("No animations in search query? Need to filter for animations first!");
+            }
+
+            if( validateLog.Length > 0)
+            {
+                Debug.Log(validateLog.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Run the shot assembly task with the given input
+        /// </summary>
+        public static void Process()
+        {
+            if( !ValidateData())
+            {
+                return;
+            }
+            var scene = UnityUtilities.CreateNewScene(model.sceneName, model.scenePath);
+            var pd = UnityUtilities.CreatePlayableDirector(model.timelineName, model.timelinePath);
+
+            InstanceActorInScene();
+            GenerateBaseTimelineTracks(pd);
+            UnityUtilities.PromptToSaveScene(scene);
+
+            // set the scene selection to our new timeline so that we can scrub and see it
+            Selection.activeObject = pd.gameObject;
+        }
+
+        /// <summary>
+        /// Load the specified actor into the scene
+        /// </summary>
+        private static void InstanceActorInScene()
+        {
+            if (model.actorPrefab == null)
+                return;
+
+            var go = GameObject.Instantiate(model.actorPrefab);
+            if (go != null)
+                model.actorSceneInstance = go;
+        }
+
+        /// <summary>
+        /// Create the playable director, timeline sequence and add all of the animation tracks to the timeline
+        /// </summary>
+        /// <param name="pd"></param>
+        private static void GenerateBaseTimelineTracks(PlayableDirector pd)
+        {
+            var timelineAsset = pd.playableAsset as TimelineAsset;
+
+            var animTrack = timelineAsset.CreateTrack<AnimationTrack>(null, "Character Rig");
+
+            // bind the track to our new actor instance
+            pd.SetGenericBinding(animTrack, model.actorSceneInstance);
+
+            // add the clips to Timeline
+            foreach (var clip in model.animationList)
+            {
+                UnityUtilities.AddClipToAnimationTrack(animTrack, clip);
+            }
+
+            var mainCam = GameObject.FindObjectOfType<Camera>();
+            var brain = mainCam.gameObject.AddComponent<CinemachineBrain>();
+
+            var go = new GameObject();
+            go.name = "LookDev VCam";
+            var vcam = go.AddComponent<CinemachineVirtualCamera>();
+            vcam.m_Follow = model.actorSceneInstance.transform;
+            vcam.m_LookAt = model.actorSceneInstance.transform;
+            // set the Body & Aim for the vcam
+            var vcamAim = vcam.AddCinemachineComponent<CinemachineComposer>();
+            var vcamBody = vcam.AddCinemachineComponent<CinemachineFramingTransposer>();
+
+            // and add a cinemachine track so we can see what we're animating
+            var cmTrack = timelineAsset.CreateTrack<CinemachineTrack>(null,  "Brain");
+
+            pd.SetGenericBinding(cmTrack, brain);
+            
+            var cmClip = cmTrack.CreateDefaultClip();
+            cmClip.duration = pd.duration;
+            cmClip.displayName = go.name;
+            var cmAsset = cmClip.asset as CinemachineShot;
+            cmAsset.VirtualCamera.exposedName = "LookdevVCam";
+            pd.SetReferenceValue("LookdevVCam", vcam);
+        }
+
+        /// <summary>
+        /// Exports the resulting scene out to a .unitypackage, including all dependencies
+        /// </summary>
+        public static void Export()
+        {
+            UnityUtilities.ExportPackage(model.scenePath, model.sceneName);
+        }
+    }
+}
---
+++ Editor/Utility/UnityUtilities.cs
@@ -0,0 +1,111 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
+namespace PixelWizards.ShotAssembly
+{
+    public class UnityUtilities
+    {
+        // generate this type of Timeline Track
+        public static Scene CreateNewScene( string sceneName, string scenePath)
+        {
+            var newScene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+            newScene.name = sceneName;
+            //EditorSceneManager.SaveScene(newScene, "Assets/" + scenePath + "/" + sceneName + ".unity");
+
+            return newScene;
+        }
+
+        public static PlayableDirector CreatePlayableDirector(string timelineName, string timelinePath)
+        {
+            // create the gameobject
+            var go = new GameObject();
+            go.name = timelineName;
+
+            // add the playable director
+            var pd = go.AddComponent<PlayableDirector>();
+
+            // create the timeline asset
+            var ta = CreateTimelineAsset(timelineName, timelinePath);
+
+            // assign the asset to the playable director
+            SetTimelinePlayableAsset(go, ta);
+
+            return pd;
+        }
+
+        public static void AddClipToAnimationTrack(TrackAsset track, AnimationClip clip)
+        {
+            var newClip = track.CreateDefaultClip();
+            newClip.duration = clip.length;
+            newClip.displayName = clip.name;
+            var animAsset = newClip.asset as AnimationPlayableAsset;
+            animAsset.clip = clip;
+        }
+
+        /// <summary>
+        /// create the timeline asset at the given path
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static TimelineAsset CreateTimelineAsset(string name, string path)
+        {
+            if (!AssetDatabase.IsValidFolder("Assets/" + path))
+            {
+                AssetDatabase.CreateFolder("Assets", path);
+            }
+            var ta = ScriptableObjectUtility.CreateAssetType<TimelineAsset>("Assets/" + path, name);
+            return ta;
+        }
+
+        /// <summary>
+        /// assign a timeline asset to a given gameobject (MUST have playable director)
+        /// </summary>
+        /// <param name="go"></param>
+        /// <param name="ta"></param>
+        public static void SetTimelinePlayableAsset( GameObject go, TimelineAsset ta)
+        {
+            var dir = go.GetComponent<PlayableDirector>();
+            if( dir == null)
+            {
+                Debug.LogError("GameObject : " + go.name + " does not have a playable director component?");
+                return;
+            }
+            dir.playableAsset = ta;
+        }
+
+
+        /// <summary>
+        /// Prompt the user to save the new scene
+        /// </summary>
+        /// <param name="thisScene"></param>
+        public static void PromptToSaveScene(Scene thisScene)
+        {
+            var path = EditorUtility.SaveFilePanelInProject("Save Scene",  thisScene.name + ".unity", "unity", "Please save the new scene.");
+
+            if (path.Length != 0)
+            {
+                EditorSceneManager.SaveScene(thisScene, path);
+            }
+        }
+
+        /// <summary>
+        /// Export the given asset at scenePath to a .unitypackage
+        /// </summary>
+        /// <param name="scenePath">Full path in the project to the asset that we want to export to a .unitypackage</param>
+        /// <param name="sceneName">name of the asset we want to export (used for the filename)</param>
+        public static void ExportPackage(string scenePath, string sceneName)
+        {
+            var path = EditorUtility.SaveFilePanel("Export Package", sceneName + ".unitypackage", "unitypackage", "Save the exported package.");
+
+            if (path.Length != 0)
+            {
+                AssetDatabase.ExportPackage(path, scenePath + sceneName, ExportPackageOptions.IncludeDependencies);
+            }
+        }
+    }
+}
